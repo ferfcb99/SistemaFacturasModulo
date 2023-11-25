88c37ea96c8373d29db51a3cf4d45042a26d01fb
ferfcb99 <fer_fcb99@outlook.com>
2023-11-24 20:38:12 -0600

    Se sube proyecto de facturas

---
+++ src/main/java/facturas/api/FacturasModuloApplication.java
@@ -0,0 +1,13 @@
+package facturas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturasModuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturasModuloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/facturas/api/FacturasModuloApplicationTests.java
@@ -0,0 +1,13 @@
+package facturas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturasModuloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
